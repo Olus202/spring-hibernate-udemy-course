65e866d57f48ba9287dd9da215ec055aae2b624b
Polus <aleksandra.polus@protonmail.com>
2020-03-25 23:19:33 +0100

    AOP - @After - part 348

---
+++ spring-demo-aop-z-after/src/com/luv2code/aopdemo/AfterReturningDemoApp.java
@@ -0,0 +1,31 @@
+package com.luv2code.aopdemo;
+
+import java.util.List;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+
+public class AfterReturningDemoApp {
+
+	public static void main(String[] args) {
+		
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+		
+		List<Account> theAccounts = theAccountDAO.findAccounts(false);
+		
+		System.out.println("After Returning App\n");
+		System.out.println(theAccounts);
+		
+		System.out.println("\n");
+		
+		for (Account acc : theAccounts) {
+			System.out.println(acc.getName());
+		}
+		
+		context.close();
+	}
+
+}
