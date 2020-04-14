93613b37ccdb66fa6c211d92ec8a6c1346e99f13
Polus <aleksandra.polus@protonmail.com>
2020-04-14 21:31:44 +0200

    Spring Security - JBDC + password encryption - part 446

--- spring-security-app/src/main/java/com/luv2code/demo/config/DemoAppConfig.java
+++ spring-security-app/src/main/java/com/luv2code/demo/config/DemoAppConfig.java
@@ -1,16 +1,32 @@
 package com.luv2code.demo.config;
 
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.luv2code.demo")
+@PropertySource("classpath:persistence-mysql.properties")
 public class DemoAppConfig {
+
+	@Autowired
+	private Environment env;
+
+	Logger logger = Logger.getLogger(getClass().getName());
 
 	@Bean
 	public ViewResolver viewResolver() {
@@ -22,5 +38,39 @@ public class DemoAppConfig {
 
 		return viewResolver;
 
+	}
+
+	@Bean
+	public DataSource securityDataSource() {
+		
+		ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+
+		try {
+			securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+		} catch (PropertyVetoException e) {
+			new RuntimeException(e);
+		}
+		
+		logger.info(">>>jdbc.url=" + env.getProperty("jdbc.url"));
+		logger.info(">>>jdbc.user=" + env.getProperty("jdbc.user"));
+
+		securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+		securityDataSource.setUser(env.getProperty("jdbc.user"));
+		securityDataSource.setPassword(env.getProperty("jdbc.password"));
+
+		securityDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+		securityDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+		securityDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+		securityDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+
+		return securityDataSource;
+
+	}
+
+	private int getIntProperty(String propName) {
+		String propVal = env.getProperty(propName);
+		int intPropVal = Integer.parseInt(propVal);
+		
+		return intPropVal;
 	}
 }
--- spring-security-app/src/main/java/com/luv2code/demo/config/DemoSecutityConfig.java
+++ spring-security-app/src/main/java/com/luv2code/demo/config/DemoSecutityConfig.java
@@ -1,25 +1,25 @@
 package com.luv2code.demo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Configuration
 @EnableWebSecurity
 public class DemoSecutityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource securityDataSource;
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
-		UserBuilder users = User.withDefaultPasswordEncoder();
-		
-		auth.inMemoryAuthentication().withUser(users.username("John").password("test123").roles("EMPLOYEE"))
-				.withUser(users.username("Mary").password("test123").roles("EMPLOYEE", "MANAGER"))
-				.withUser(users.username("Sam").password("test123").roles("EMPLOYEE", "ADMIN"));
+		auth.jdbcAuthentication().dataSource(securityDataSource);
 	}
 
 	@Override
